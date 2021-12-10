4e2a5b168e832699d247c33c5eb1eb2cee7ca079
kurousa <r.takahashi.1026@gmail.com>
2021-12-10 22:58:41 +0900

    implements header styled components

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAvac0s2GBmPnuwxbmsu567pnJc0iA7XYo",
+    authDomain: "slack-clone-c8c10.firebaseapp.com",
+    projectId: "slack-clone-c8c10",
+    storageBucket: "slack-clone-c8c10.appspot.com",
+    messagingSenderId: "912987623678",
+    appId: "1:912987623678:web:8e5ce1c0720eaa75af55af"
+  };

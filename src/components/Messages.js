b1cedaf9e5825d74389b6f73362be872a4641ea7
kurousa <r.takahashi.1026@gmail.com>
2021-12-16 23:59:19 +0900

    add Message component initalize

---
+++ src/components/Messages.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Messages({ message, timestamp, user, userImage }) {
+    return (
+        <MessageContainer>
+            
+        </MessageContainer>
+    )
+}
+
+export default Messages;
+
+const MessageContainer = styled.div`
+
+`;

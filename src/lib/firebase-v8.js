d55cc8a20d377edee9c8582700b3194744e6e8b1
kurousa <r.takahashi.1026@gmail.com>
2021-12-17 12:32:11 +0900

    mod: referense environmet values

--- src/lib/firebase-v8.js
+++ src/lib/firebase-v8.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAvac0s2GBmPnuwxbmsu567pnJc0iA7XYo",
-    authDomain: "slack-clone-c8c10.firebaseapp.com",
-    projectId: "slack-clone-c8c10",
-    storageBucket: "slack-clone-c8c10.appspot.com",
-    messagingSenderId: "912987623678",
-    appId: "1:912987623678:web:8e5ce1c0720eaa75af55af"
+    apiKey: process.env.REACT_APP_API_KEY, //"AIzaSyAvac0s2GBmPnuwxbmsu567pnJc0iA7XYo",
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN, //"slack-clone-c8c10.firebaseapp.com",
+    projectId: process.env.REACT_APP_AUTH_PROJECTID, //"slack-clone-c8c10",
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET, //"slack-clone-c8c10.appspot.com",
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID, //"912987623678",
+    appId: process.env.REACT_APP_APP_ID,// "1:912987623678:web:8e5ce1c0720eaa75af55af"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);

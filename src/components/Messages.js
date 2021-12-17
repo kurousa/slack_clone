7da24e4882dfe70d769c861cd82a2cdb09597540
kurousa <r.takahashi.1026@gmail.com>
2021-12-17 21:09:41 +0900

    implements view for Messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -4,7 +4,15 @@ import styled from 'styled-components';
 function Messages({ message, timestamp, user, userImage }) {
     return (
         <MessageContainer>
-            
+            <img src={userImage} alt=""/>
+            <MessageInfo>
+                <h4>
+                    {user} <span>{new Date(timestamp?.toDate()).toLocaleString()}</span>
+                </h4>
+                <p>
+                    {message}
+                </p>
+            </MessageInfo>
         </MessageContainer>
     )
 }
@@ -12,5 +20,23 @@ function Messages({ message, timestamp, user, userImage }) {
 export default Messages;
 
 const MessageContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
 
+    > img {
+        height: 50px;
+        border-radius: 8px;
+    }
+`;
+
+const MessageInfo = styled.div`
+    padding-left: 10px;
+
+    > h4 > span {
+        color: gray;
+        font-weight: 300;
+        margin-left: 4px;
+        font-size: 10px;
+    }
 `;
